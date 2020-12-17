7689b2de891278b096d140c0ef29de05a1c0071e
Ivan Legorreta <ilegorreta@hotmail.com>
2020-12-17 11:36:06 -0500

    Refactoring

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+'''
+Developed by: Ivan Legorreta
+Contact information: ilegorreta@outlook.com
+'''
+
 import nibabel as nib
 import os
 import numpy as np
